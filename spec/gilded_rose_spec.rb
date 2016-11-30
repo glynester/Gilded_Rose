bcba9e3a35a3b15f68123798b6b53c6574897d4d
glynester <glynester@hotmail.com>
2016-11-30 14:21:41 +0000

    Added tests for more scenarios and failing test for new category

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -15,16 +15,82 @@ describe GildedRose do
     end
   end
 
-
   context 'Add "Aged Brie"' do
-    it 'shows correct balance' do
+    it 'shows correct balance on the following day (days down by 1, quality up by 1)' do
       brie = Item.new("Aged Brie", 20, 30)
       guilded_rose = GildedRose.new(brie)
       guilded_rose.update_quality
       expect(brie.to_s).to eq "Aged Brie, 19, 31"
     end
+    it 'does not exceed 50' do
+      brie = Item.new("Aged Brie", 20, 30)
+      guilded_rose = GildedRose.new(brie)
+      40.times { guilded_rose.update_quality }
+      expect(brie.to_s).to eq "Aged Brie, -20, 50"
+    end
+  end
+
+  context 'Add "Sulfuras"' do
+    it 'shows correct balance on the following day (days + quality the same)' do
+      sulfuras = Item.new("Sulfuras, Hand of Ragnaros", 20, 30)
+      guilded_rose = GildedRose.new(sulfuras)
+      guilded_rose.update_quality
+      expect(sulfuras.to_s).to eq "Sulfuras, Hand of Ragnaros, 20, 30"
+    end
+    it 'stays the same' do
+      sulfuras = Item.new("Sulfuras, Hand of Ragnaros", 20, 30)
+      guilded_rose = GildedRose.new(sulfuras)
+      40.times { guilded_rose.update_quality }
+      expect(sulfuras.to_s).to eq "Sulfuras, Hand of Ragnaros, 20, 30"
+    end
+  end
+
+  context 'Add "Sulfuras"' do
+    it 'shows correct balance on the following day  (days down by 1, quality up by 1)' do
+      b_stage_passes = Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 40)
+      guilded_rose = GildedRose.new(b_stage_passes)
+      guilded_rose.update_quality
+      expect(b_stage_passes.to_s).to eq "Backstage passes to a TAFKAL80ETC concert, 19, 41"
+    end
+    it 'does not exceed 50' do
+      b_stage_passes = Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 40)
+      guilded_rose = GildedRose.new(b_stage_passes)
+      20.times { guilded_rose.update_quality }
+      expect(b_stage_passes.to_s).to eq "Backstage passes to a TAFKAL80ETC concert, 0, 50"
+    end
+    it 'reduces to zero when days countdown < zero' do
+      b_stage_passes = Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 40)
+      guilded_rose = GildedRose.new(b_stage_passes)
+      21.times { guilded_rose.update_quality }
+      expect(b_stage_passes.to_s).to eq "Backstage passes to a TAFKAL80ETC concert, -1, 0"
+    end
+  end
+
+  context 'Addin other types of non-special items' do
+    it 'shows correct balance on the following day  (days down by 1, quality down by 1)' do
+      normal_item = Item.new("Normal Item", 20, 40)
+      guilded_rose = GildedRose.new(normal_item)
+      guilded_rose.update_quality
+      expect(normal_item.to_s).to eq "Normal Item, 19, 39"
+    end
+
+    it 'quality reduces 2 times faster once "sell by" is passed (days down by 1, quality down by 2)' do
+      normal_item = Item.new("Normal Item", 1, 40)
+      guilded_rose = GildedRose.new(normal_item)
+      15.times { guilded_rose.update_quality }
+      expect(normal_item.to_s).to eq "Normal Item, -14, 11"
+    end
 
   end
+
+  # context 'Add "Conjured"' do
+  #   it 'shows correct balance on the following day  (days down by 1, quality down by 2)' do
+  #     conjured = Item.new("Conjured", 20, 40)
+  #     guilded_rose = GildedRose.new(conjured)
+  #     guilded_rose.update_quality
+  #     expect(conjured.to_s).to eq "Conjured, 19, 38"
+  #   end
+  # end
 end
 
 
